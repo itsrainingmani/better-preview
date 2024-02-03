c7cfcf350eda56f52707afdb03293b7f967c58ee
Mani Sundararajan <10191300+itsrainingmani@users.noreply.github.com>
2024-02-02 22:52:16 -0500

    format the image to look more tweety

--- pages/api/twitter.tsx
+++ pages/api/twitter.tsx
@@ -6,6 +6,36 @@ export const config = {
   runtime: "edge",
 };
 
+const MAX_TWEET_LENGTH = 280;
+
+function decodeHtmlEntities(html: string) {
+  return html.replace(/&#(\d+);|&([^;\s]+);/g, (match, decimal, named) => {
+    if (named) {
+      // Handle named entities
+      const map: Record<string, string> = {
+        amp: "&",
+        lt: "<",
+        gt: ">",
+        quot: '"',
+        apos: "'",
+        nbsp: "\u00A0",
+        ndash: "–",
+        mdash: "—",
+        lsquo: "‘",
+        rsquo: "’",
+        sbquo: "‚",
+        ldquo: "“",
+        rdquo: "”",
+        bdquo: "„",
+      };
+      return map[named] || match;
+    } else {
+      // Handle decimal entities
+      return String.fromCharCode(decimal);
+    }
+  });
+}
+
 export default async function handler(request: VercelRequest) {
   try {
     const fontData = await fetch(
@@ -15,18 +45,39 @@ export default async function handler(request: VercelRequest) {
 
     // ?title=<title>
     const hasTitle = searchParams.has("url");
-    const title = hasTitle
-      ? searchParams.get("url")?.slice(0, 100)
-      : "https://twitter.com/KateKozuch/status/1753425540025856483";
+    let image_text = "";
+    if (hasTitle) {
+      const title = hasTitle
+        ? searchParams.get("url")?.slice(0, 100)
+        : "https://twitter.com/KateKozuch/status/1753425540025856483";
 
-    const twitter_oembed = `https://publish.twitter.com/oembed?url=${title}&omit_script=1&lang=en`;
+      const twitter_oembed = `https://publish.twitter.com/oembed?url=${title}&omit_script=1&lang=en`;
 
-    const resp = await fetch(twitter_oembed);
-    let data = await resp.json();
+      const resp = await fetch(twitter_oembed);
+      let data = await resp.json();
 
-    const domstring = parse(data["html"]);
-    const blockquote =
-      domstring.getElementsByTagName("blockquote")[0].innerText;
+      const domstring = parse(data["html"]);
+      image_text = decodeHtmlEntities(
+        domstring.getElementsByTagName("blockquote")[0].innerText,
+      );
+      console.log(image_text);
+
+      let last_emdash = image_text.lastIndexOf("— ");
+
+      let display_name =
+        image_text.slice(last_emdash + 2, image_text.length).split(")")[0] +
+        ")";
+      image_text = image_text.slice(0, last_emdash);
+
+      if (image_text.length >= MAX_TWEET_LENGTH) {
+        image_text = image_text.slice(0, MAX_TWEET_LENGTH - 3);
+        // image_text += "...";
+      }
+
+      image_text += `\n\n— ${display_name}`;
+    } else {
+      image_text = "twitter";
+    }
 
     return new ImageResponse(
       (
@@ -37,27 +88,26 @@ export default async function handler(request: VercelRequest) {
             height: "100%",
             width: "100%",
             display: "flex",
-            textAlign: "center",
             alignItems: "center",
             justifyContent: "center",
             flexDirection: "column",
-            flexWrap: "nowrap",
+            flexWrap: "wrap",
           }}
         >
           <div
             style={{
-              fontSize: 60,
+              fontSize: 40,
               fontStyle: "normal",
               letterSpacing: "-0.025em",
               color: "white",
-              // marginTop: 30,
               padding: "0 60px",
-              lineHeight: 1.4,
-              whiteSpace: "pre-wrap",
+              lineHeight: 1.2,
+              wordWrap: "break-word",
+              whiteSpace: "pre-line",
               fontFamily: "Berkeley Mono",
             }}
           >
-            {blockquote}
+            {image_text}
           </div>
         </div>
       ),
