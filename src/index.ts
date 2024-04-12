308e6486f8e2c8e6cd8a20976bd812534b0dee1f
Mani Sundararajan <10191300+itsrainingmani@users.noreply.github.com>
2024-04-12 15:36:15 -0400

    set the title to be the tweet contents

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,43 @@
-import { Hono } from "hono";
-import { html } from "hono/html";
-import { parse } from "node-html-parser";
-import { FC } from "hono/jsx";
+import { Hono } from 'hono';
+import { html } from 'hono/html';
+import { parse } from 'node-html-parser';
+import { FC } from 'hono/jsx';
 
 const app = new Hono();
 
-app.get("/", async (c) => {
-  let image_api_url = c.env.TWIT_IMAGE_URL;
+app.get('/', async (c) => {
+	let image_api_url = c.env.TWIT_IMAGE_URL;
+	let tweet_api_url = c.env.TWIT_CONTENT_URL;
 
-  const tweet_param = c.req.query("tweet");
-  if (tweet_param) {
-    const twit_image = `${image_api_url}${tweet_param}`;
+	const tweet_param = c.req.query('tweet');
+	if (tweet_param) {
+		const twit_image = `${image_api_url}${tweet_param}`;
+		const twit_contents = `${tweet_api_url}${tweet_param}`;
 
-    return c.html(
-      html`<!doctype html>
+		return c.html(
+			html`<!doctype html>
   <head>
     <meta property="og:type" content="website" />
-    <meta property="og:title" content="Twit" />
+    <meta property="og:title" content="${twit_contents}" />
     <meta property="og:url" content="${tweet_param}" />
     <meta property="og:image" content="${twit_image}" />
     <script type="text/javascript">
       document.addEventListener("DOMContentLoaded", (event) => {
-        window.location.href = "${tweet_param}";  
+        window.location.href = "${tweet_param}";
       });
     </script>
   </head>
   <body>
   </body>
-</html>`,
-    );
-  } else {
-    return c.text("Tweet not supplied", 500);
-  }
+</html>`
+		);
+	} else {
+		return c.text('Tweet not supplied', 500);
+	}
 });
 
-app.get("/notfound", (c) => {
-  return c.notFound();
+app.get('/notfound', (c) => {
+	return c.notFound();
 });
 
 export default app;
