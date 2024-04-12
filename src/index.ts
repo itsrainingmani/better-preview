79fbe99d17f58de6304860a405f3a167c1d5ab38
Mani Sundararajan <10191300+itsrainingmani@users.noreply.github.com>
2024-04-12 15:49:39 -0400

    fetch the tweet contents separately?

--- src/index.ts
+++ src/index.ts
@@ -12,13 +12,19 @@ app.get('/', async (c) => {
 	const tweet_param = c.req.query('tweet');
 	if (tweet_param) {
 		const twit_image = `${image_api_url}${tweet_param}`;
-		const twit_contents = `${tweet_api_url}${tweet_param}`;
+		const twit_resp = await fetch(`${tweet_api_url}${tweet_param}`);
+		const twit_data: any = await twit_resp.json();
+		let twit_contents;
+
+		if (twit_data) {
+			twit_contents = twit_data['content'];
+		}
 
 		return c.html(
 			html`<!doctype html>
   <head>
     <meta property="og:type" content="website" />
-    <meta property="og:title" content="${twit_contents}" />
+    <meta property="og:title" content="${twit_contents ?? 'Twit'}" />
     <meta property="og:url" content="${tweet_param}" />
     <meta property="og:image" content="${twit_image}" />
     <script type="text/javascript">
