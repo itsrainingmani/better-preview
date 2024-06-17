307e1d7a5117075f267311933bc67cec09d8720a
Mani Sundararajan <10191300+itsrainingmani@users.noreply.github.com>
2024-06-17 14:32:42 -0400

    reduce image size to 800x400

--- pages/api/twitter.tsx
+++ pages/api/twitter.tsx
@@ -128,8 +128,8 @@ export default async function handler(request: VercelRequest) {
 				</div>
 			),
 			{
-				width: 1200,
-				height: 630,
+				width: 800,
+				height: 400,
 				fonts: [
 					{
 						name: 'Berkeley Mono',
