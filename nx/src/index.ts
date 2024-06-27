61a6d6aac739bc301b4b981b2fddf5dc435eb3bd
Mani Sundararajan <10191300+itsrainingmani@users.noreply.github.com>
2024-06-27 16:09:08 -0400

    add option to generate urls similar to twitter
    also simplify the code by removing the twitter contents stuff

--- nx/src/index.ts
+++ nx/src/index.ts
@@ -1,30 +1,13 @@
-import { Hono } from 'hono';
-import { html } from 'hono/html';
-import { parse } from 'node-html-parser';
-import { FC } from 'hono/jsx';
+import { Hono } from "hono";
+import { html } from "hono/html";
 
 const app = new Hono();
 
-app.get('/', async (c) => {
-	let image_api_url = c.env.TWIT_IMAGE_URL;
-	let tweet_api_url = c.env.TWIT_CONTENT_URL;
-
-	const tweet_param = c.req.query('tweet');
-	if (tweet_param) {
-		const twit_image = `${image_api_url}${tweet_param}`;
-		const twit_resp = await fetch(`${tweet_api_url}${tweet_param}`);
-		let twit_contents: string | undefined = undefined;
-
-		if (twit_resp.ok) {
-			twit_contents = await twit_resp.text();
-		}
-
-		return c.html(
-			html`<!doctype html>
+function gen_meta(tweet_param: string, twit_image: string): any {
+	return html`<!doctype html>
   <head>
     <meta property="og:type" content="website" />
     <meta property="og:title" content="Twit" />
-		<meta property="og:description" content="${twit_contents}" />
     <meta property="og:url" content="${tweet_param}" />
     <meta property="og:image" content="${twit_image}" />
 		<meta property="og:image:width" content="800" />
@@ -37,14 +20,34 @@ app.get('/', async (c) => {
   </head>
   <body>
   </body>
-</html>`
-		);
+</html>`;
+}
+
+app.get("/", async (c) => {
+	let image_api_url = c.env.TWIT_IMAGE_URL;
+
+	const tweet_param = c.req.query("tweet");
+	if (tweet_param) {
+		const twit_image = `${image_api_url}${tweet_param}`;
+
+		return c.html(gen_meta(tweet_param, twit_image));
 	} else {
-		return c.text('Tweet not supplied', 500);
+		return c.text("Tweet not supplied", 500);
 	}
 });
 
-app.get('/notfound', (c) => {
+// tweets are in the format - https://x.com/:username/status/:tweet_id
+app.get("/:username/status/:tweet_id", async (c) => {
+	let image_api_url = c.env.TWIT_IMAGE_URL;
+
+	const { username, tweet_id } = c.req.param();
+	const tweet_param = `https://x.com/${username}/status/${tweet_id}`;
+	const twit_image = `${image_api_url}${tweet_param}`;
+
+	return c.html(gen_meta(tweet_param, twit_image));
+});
+
+app.get("/notfound", (c) => {
 	return c.notFound();
 });
 
