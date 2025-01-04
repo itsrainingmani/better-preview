0bb2e4afcaf0ee13d854a2623014288603e97a02
Mani Sundararajan <10191300+itsrainingmani@users.noreply.github.com>
2025-01-04 02:09:45 -0500

    move help html to a separate file

--- nx/src/index.ts
+++ nx/src/index.ts
@@ -1,18 +1,20 @@
 import { Hono } from "hono";
-import { html } from "hono/html";
-import { help_msg } from "./help";
+import { html, raw } from "hono/html";
+import helpContent from "./help.html";
 
 const app = new Hono();
 
 function gen_meta(
-	username: string | undefined,
-	tweet_param: string,
-	twit_image: string,
+  username: string | undefined,
+  tweet_param: string,
+  twit_image: string
 ) {
-	return html`<!doctype html>
+  return html`<!doctype html>
   <head>
     <meta property="og:type" content="website" />
-    <meta property="og:title" content="${username ? `by @${username}` : "Twit"}" />
+    <meta property="og:title" content="${
+      username ? `by @${username}` : "Twit"
+    }" />
     <meta property="og:url" content="${tweet_param}" />
     <meta property="og:image" content="${twit_image}" />
 		<meta property="og:image:width" content="800" />
@@ -29,30 +31,30 @@ function gen_meta(
 }
 
 app.get("/", async (c) => {
-	const image_api_url = c.env?.TWIT_IMAGE_URL;
+  const image_api_url = c.env?.TWIT_IMAGE_URL;
 
-	const tweet_param = c.req.query("tweet");
-	if (tweet_param) {
-		const twit_image = `${image_api_url}${tweet_param}`;
+  const tweet_param = c.req.query("tweet");
+  if (tweet_param) {
+    const twit_image = `${image_api_url}${tweet_param}`;
 
-		return c.html(gen_meta(undefined, tweet_param, twit_image));
-	}
-	return c.html(help_msg());
+    return c.html(gen_meta(undefined, tweet_param, twit_image));
+  }
+  return c.html(raw(helpContent));
 });
 
 // tweets are in the format - https://x.com/:username/status/:tweet_id
 app.get("/:username/status/:tweet_id", async (c) => {
-	const image_api_url = c.env?.TWIT_IMAGE_URL;
+  const image_api_url = c.env?.TWIT_IMAGE_URL;
 
-	const { username, tweet_id } = c.req.param();
-	const tweet_param = `https://x.com/${username}/status/${tweet_id}`;
-	const twit_image = `${image_api_url}${tweet_param}`;
+  const { username, tweet_id } = c.req.param();
+  const tweet_param = `https://x.com/${username}/status/${tweet_id}`;
+  const twit_image = `${image_api_url}${tweet_param}`;
 
-	return c.html(gen_meta(username, tweet_param, twit_image));
+  return c.html(gen_meta(username, tweet_param, twit_image));
 });
 
 app.get("/notfound", (c) => {
-	return c.notFound();
+  return c.notFound();
 });
 
 export default app;
